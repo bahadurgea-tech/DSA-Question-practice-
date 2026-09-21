3c791177d78e72be33573320c6ea1c0d89715d48
shubham_bahadurge <bahadurgea@gmail.com>
2026-09-21 09:25:25 +0530

    end file

--- lr214.cpp
+++ lr214.cpp
@@ -3,5 +3,16 @@
 #include<cstring>
 using namespace std;
 int main (){
+    //copy string
+    char rt []="hi subhiii";
+    strcpy(rt,"shubhu");
+    cout<<rt<<endl;
+    //add two string
+  
+    char str1 [ ]= "shubh";
+    char str2 [] = "iii";
+    strcat(str1,str2);
+    cout<<str1<<endl;
+    cout<<str2;
 
 }
