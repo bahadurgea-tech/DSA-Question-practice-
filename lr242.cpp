06ff048ee44995d12e1c7513fd65f98bb258de67
shubham_bahadurge <bahadurgea@gmail.com>
2026-09-24 14:26:56 +0530

    add 2d dynamic array

--- lr242.cpp
+++ lr242.cpp
@@ -1,5 +1,20 @@
+//2D dynamic array
 #include<iostream>
 using namespace std;
 int main (){
-    
+    int row = 3;
+    int col = 4;
+    int ** matrx = new int * [row];
+    for (int i =0;i<row;i++){
+        matrx[i] = new int [col];
+    }
+//store a element 
+int x = 1 ;
+for (int i =0; i<row;i++){
+    for (int j = 0 ; j<col;j++){
+        matrx[i][j]=x++;
+        cout<<matrx[i][j]<<" ";
+    }
+    cout<<endl;
+}    
 }
